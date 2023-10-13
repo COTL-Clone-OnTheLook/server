e49b314ac74243c8bfe4836c6ccb5a6843dacb50
SeYoung Jeong <65009713+JSY8869@users.noreply.github.com>
2023-10-13 15:04:43 +0900

    init: 프로젝트 초기 생성

---
+++ api/src/test/java/cotl/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cotl.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
